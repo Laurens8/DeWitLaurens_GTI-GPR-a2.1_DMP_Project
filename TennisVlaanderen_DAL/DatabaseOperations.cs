f1fb2ff28683d60b653a4a5ee8f0c63ae17295be
de wit laurens <r0901293@student.thomasmore.be>
2022-12-07 02:23:49 +0100

    First commit

---
+++ TennisVlaanderen_DAL/DatabaseOperations.cs
@@ -0,0 +1,31 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TennisVlaanderen_DAL
+{
+    public class DatabaseOperations
+    {
+        private static DatabaseConnectie _db;
+
+        private static void Start()
+        {
+            _db = new DatabaseConnectie();
+            _db.Open();
+        }      
+
+        public static List<Speler> OphalenSpelers(int spelerID)
+        {
+            Start();
+
+            var result = _db.Connectie.Query<Speler>("SELECT naam FROM TennisVlaanderen.Speler;", param: new { id = spelerID }).ToList();
+
+            _db.Close();
+
+            return result;
+        }       
+    }
+}
---
+++ TennisVlaanderen_DAL/IDatabaseConnectie.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TennisVlaanderen_DAL
+{
+    public interface IDatabaseConnectie
+    {
+        void Connecteren();
+        IDbConnection Connectie { get; set; }
+        void Open();
+        void Close();
+    }
+
+    public class DatabaseConnectie : IDatabaseConnectie
+    {
+        public IDbConnection Connectie { get; set; }
+
+        public DatabaseConnectie()
+        {
+            Connecteren();
+        }
+
+        public void Connecteren()
+        {
+            try
+            {
+                //Connectie = new SqlConnection(ConfigurationManager.ConnectionStrings["Data Source=(localDB)\MSSQLLocalDB;Initial Catalog=Publishers;Integrated Security=True;Pooling=False"].ConnectionString);
+                Connectie = new SqlConnection("server=(localdb)\\mssqllocaldb;Database=Publishers;MultipleActiveResultSets=True;");
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.ToString());
+            }
+        }
+        public void Open()
+        {
+            if (Connectie == null) Connecteren();
+
+            Connectie.Open();
+        }
+
+        public void Close()
+        {
+            if (Connectie != null)
+                Connectie.Close();
+        }
+
+    }
+}
